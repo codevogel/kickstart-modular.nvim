496971f70ac22715d2e14fec5029f4ce2f638724
Kamiel de Visser <kamieldevisser@gmail.com>
2025-10-15 15:09:51 +0200

    add keymap to open oil

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -2,5 +2,6 @@ return {
   'stevearc/oil.nvim',
   config = function()
     require('oil').setup()
+    vim.keymap.set('n', '<leader>o', ':Oil<CR>', { desc = 'Open Oil.nvim file explorer' })
   end,
 }
