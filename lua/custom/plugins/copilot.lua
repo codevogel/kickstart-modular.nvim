bb5dfe93e3cd50faccba2d9936af459089010d34
Kamiel de Visser <kamieldevisser@gmail.com>
2025-10-19 23:23:01 +0200

    feat(plugins): add GitHub Copilot and Copilot Chat integration with keymaps

---
+++ lua/custom/plugins/copilot.lua
@@ -0,0 +1,27 @@
+return {
+  'github/copilot.vim',
+  config = function()
+    vim.g.copilot_no_tab_map = true
+  end,
+  keys = {
+    { '<S-Tab>', 'copilot#Accept("\\CR")', expr = true, mode = 'i', desc = 'Accept Copilot suggestion' },
+  },
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    dependencies = {
+      { 'nvim-lua/plenary.nvim', branch = 'master' },
+    },
+    build = 'make tiktoken',
+    opts = {
+      -- See Configuration section for options
+    },
+    keys = {
+      { '<leader>zc', '<cmd>CopilotChat<CR>', mode = 'n', desc = 'Open Copilot Chat' },
+      { '<leader>ze', '<cmd>CopilotChatExplain<CR>', mode = 'n', desc = 'Explain code with Copilot Chat' },
+      { '<leader>zr', '<cmd>CopilotChatReview<CR>', mode = 'n', desc = 'Review code with Copilot Chat' },
+      { '<leader>zf', '<cmd>CopilotChatFix<CR>', mode = 'n', desc = 'Fix code with Copilot Chat' },
+      { '<leader>zo', '<cmd>CopilotChatOptimize<CR>', mode = 'n', desc = 'Optimize code with Copilot Chat' },
+      { '<leader>zd', '<cmd>CopilotChatDocs<CR>', mode = 'n', desc = 'Generate docs with Copilot Chat' },
+    },
+  },
+}
