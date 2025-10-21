cf31a59caecfde4aa656be5cd3496b363cfaa419
Kamiel de Visser <kamieldevisser@gmail.com>
2025-10-21 15:05:57 +0200

    change oil keymaps

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -2,6 +2,20 @@ return {
   'stevearc/oil.nvim',
   config = function()
     require('oil').setup()
-    vim.keymap.set('n', '<leader>o', ':Oil<CR>', { desc = 'Open Oil.nvim file explorer' })
+  end,
+  keys = function()
+    local oil = require 'oil'
+    return {
+      { '<leader>oo', ':Oil<CR>', 'n', desc = 'Open Oil.nvim file explorer' },
+      { '<leader>oc', ':Oil .<CR>', 'n', desc = 'Open Oil.nvim file explorer in CWD' },
+      {
+        '<leader>oh',
+        function()
+          oil:toggle_hidden()
+        end,
+        'n',
+        desc = 'Open Oil.nvim file explorer',
+      },
+    }
   end,
 }
